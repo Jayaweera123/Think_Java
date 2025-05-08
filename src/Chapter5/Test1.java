33e806498f4c085b1ca13cef796f93cdc5147276
Jayaweera123 <155569167+Jayaweera123@users.noreply.github.com>
2025-05-08 12:10:33 +0530

    if statement

---
+++ src/Chapter5/Test1.java
@@ -0,0 +1,24 @@
+package Chapter5;
+import java.util.Scanner;
+public class Test1 {
+    public static void main(String [] args){
+        int number;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter any number:");
+        number=scanner.nextInt();
+
+        if ( number==0 ) {
+            System.out.println("The number is zero.");
+        }
+
+        if ( number>0 ) {
+            System.out.println("The number is positive.");
+        }
+
+        if ( number<0 ) {
+            System.out.println("The number is negative.");
+        }
+
+    }
+}
